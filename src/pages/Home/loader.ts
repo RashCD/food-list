8146f168a3e27cb8ae79ae281e2f1c43da5733f1
RashCD <mrrash91@gmail.com>
2024-02-20 01:31:42 +0800

    adding data fetching using RQ in loader

--- src/pages/Home/loader.ts
+++ src/pages/Home/loader.ts
@@ -1,46 +1,54 @@
 import { LoaderFunctionArgs } from 'react-router-dom';
 import axios from 'redaxios';
+import { queryClient } from '../../main';
+import {
+	CategoriesAPITypes,
+	FoodListAPITypes,
+	PAGE_SIZE,
+	PAGE_START_INDEX,
+} from './constant';
 
-export type FoodListAPITypes = {
-	foods: {
-		id: string;
-		index: number;
-		rating: number;
-		promotion: string;
-		isNew: boolean;
-		categoryId: string;
-		minCookTime: number;
-		maxCookTime: number;
-		restaurant: string;
-		name: string;
-		imageUrl: string;
-	}[];
-};
+const categoriesQuery = () => ({
+	queryKey: ['categories'],
+	queryFn: async () =>
+		await axios.get<CategoriesAPITypes>(
+			'https://run.mocky.io/v3/b88ec762-2cb3-4015-8960-2839b06a7593',
+		),
+});
 
-export type CategoriesAPITypes = {
-	id: string;
-	name: string;
-}[];
+const foodListQuery = () => ({
+	queryKey: ['foodList'],
+	queryFn: async () =>
+		await axios.get<FoodListAPITypes>(
+			'https://run.mocky.io/v3/c75dc0d8-ad78-4b3d-b697-807a5ded8645',
+		),
+});
 
 export const homeLoader = async ({ request }: LoaderFunctionArgs) => {
 	const url = new URL(request.url);
 	const q = url.searchParams.get('q');
 	const tab = url.searchParams.get('tab');
+	const page = url.searchParams.get('page') || '1';
+	let filteredFoodList: FoodListAPITypes['foods'] = [];
+	let foodCountDifferences = 0;
+	let totalCount = filteredFoodList.length;
 
-	console.log({ tab });
+	const categories = await queryClient.fetchQuery({
+		...categoriesQuery(),
+	});
 
-	const categories = await axios.get<CategoriesAPITypes>(
-		'https://run.mocky.io/v3/b88ec762-2cb3-4015-8960-2839b06a7593',
-	);
+	const foodList = await queryClient.fetchQuery({
+		...foodListQuery(),
+	});
 
-	const foodList = await axios.get<FoodListAPITypes>(
-		'https://run.mocky.io/v3/c75dc0d8-ad78-4b3d-b697-807a5ded8645',
-	);
+	filteredFoodList = foodList.data.foods;
+	totalCount = filteredFoodList.length;
 
 	if (q) {
-		foodList.data.foods = foodList.data.foods.filter((food) =>
+		filteredFoodList = filteredFoodList.filter((food) =>
 			food.name.toLowerCase().includes(q.toLowerCase()),
 		);
+		totalCount = filteredFoodList.length;
 	}
 
 	if (tab && tab !== 'All') {
@@ -48,14 +56,30 @@ export const homeLoader = async ({ request }: LoaderFunctionArgs) => {
 			(category) => category.name === tab,
 		);
 
-		foodList.data.foods = foodList.data.foods.filter(
+		filteredFoodList = filteredFoodList.filter(
 			(food) => food.categoryId === category?.id,
 		);
+
+		totalCount = filteredFoodList.length;
+	}
+
+	if (page) {
+		const pageNumber = parseInt(page, 10);
+		const startIndex = PAGE_START_INDEX;
+		const prevEndIndex = (pageNumber - 1) * PAGE_SIZE;
+		const endIndex = pageNumber * PAGE_SIZE;
+		filteredFoodList = filteredFoodList.slice(startIndex, endIndex);
+		foodCountDifferences =
+			filteredFoodList.length -
+			filteredFoodList.slice(startIndex, prevEndIndex).length;
 	}
 
 	return {
 		categories: categories.data,
-		foodList: foodList.data.foods,
+		foodList: filteredFoodList,
+		hasMore:
+			filteredFoodList.length !== totalCount &&
+			foodCountDifferences >= PAGE_SIZE,
 		q,
 	};
 };
