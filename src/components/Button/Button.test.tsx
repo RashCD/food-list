3f29c9b5511bc42e1005cf0734123bab8a0ebba3
RashCD <mrrash91@gmail.com>
2024-02-20 04:09:04 +0800

    add button test

---
+++ src/components/Button/Button.test.tsx
@@ -0,0 +1,42 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import Button from './Button';
+import { vi } from 'vitest';
+
+describe('Button component', () => {
+	it('should render', () => {
+		render(<Button>Button</Button>);
+
+		expect(screen.getByText('Button')).toBeInTheDocument();
+
+		screen.debug();
+	});
+
+	it('should be able to click button', () => {
+		const handleClick = vi.fn();
+
+		render(<Button onClick={handleClick}>Button</Button>);
+
+		fireEvent.click(screen.getByText('Button'));
+
+		expect(handleClick).toHaveBeenCalled();
+		expect(handleClick).toHaveBeenCalledTimes(1);
+	});
+
+	it('should be able to accept variant primary', () => {
+		const comp = render(<Button variant="primary">Button</Button>);
+
+		expect(comp).toMatchSnapshot();
+	});
+
+	it('should be able to accept variant outline', () => {
+		const comp = render(<Button variant="outline">Button</Button>);
+
+		expect(comp).toMatchSnapshot();
+	});
+
+	it('should be able to accept variant ghost', () => {
+		const comp = render(<Button variant="ghost">Button</Button>);
+
+		expect(comp).toMatchSnapshot();
+	});
+});
