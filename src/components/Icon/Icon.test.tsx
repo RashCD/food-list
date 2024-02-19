5e7788b3f5e7d6f6f542066c6d9843de7d11976c
RashCD <mrrash91@gmail.com>
2024-02-20 04:47:34 +0800

    add icon test

---
+++ src/components/Icon/Icon.test.tsx
@@ -0,0 +1,64 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import Icon from './Icon';
+import { vi } from 'vitest';
+
+describe('Icon component', () => {
+	it('should render', () => {
+		render(<Icon source="https://some-image-url" altText="Alt text" />);
+
+		expect(screen.getByAltText('Alt text')).toBeInTheDocument();
+
+		screen.debug();
+	});
+
+	it('should be able to render image', () => {
+		render(<Icon source="https://some-image-url" altText="Alt text" />);
+
+		expect(screen.getByRole('img')).toBeInTheDocument();
+		expect(screen.getByRole('img')).toHaveAttribute(
+			'src',
+			'https://some-image-url',
+		);
+	});
+
+	it('should be able to click only when onClick is provided', () => {
+		const handleClick = vi.fn();
+
+		render(
+			<Icon
+				source="https://some-image-url"
+				altText="Alt text"
+				onClick={handleClick}
+			/>,
+		);
+
+		fireEvent.click(screen.getByRole('img'));
+
+		expect(handleClick).toHaveBeenCalled();
+		expect(handleClick).toHaveBeenCalledTimes(1);
+	});
+
+	it('should not be able to click when onClick is not provided', () => {
+		const handleClick = vi.fn();
+
+		render(<Icon source="https://some-image-url" altText="Alt text" />);
+
+		fireEvent.click(screen.getByRole('img'));
+
+		expect(handleClick).toHaveBeenCalledTimes(0);
+	});
+
+	it('should match snapshot', () => {
+		const handleClick = vi.fn();
+
+		const component = render(
+			<Icon
+				source="https://some-image-url"
+				altText="Alt text"
+				onClick={handleClick}
+			/>,
+		);
+
+		expect(component).toMatchSnapshot();
+	});
+});
