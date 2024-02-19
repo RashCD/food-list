d449ddd74a60ecc75d895f0597d781251c2e2c1a
RashCD <mrrash91@gmail.com>
2024-02-19 18:48:31 +0800

    implement router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Home from './pages/Home/Home';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import NotFound from './pages/NotFound/NotFound';
+import { homeLoader } from './pages/Home/loader';
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <Home />,
+		errorElement: <NotFound />,
+		loader: homeLoader,
+	},
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>,
+);
