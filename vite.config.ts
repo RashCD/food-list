f1fc1d7eda01e5347e682e91cfc914564ebf526b
RashCD <mrrash91@gmail.com>
2024-02-20 03:59:13 +0800

    setup react testing lib test

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import type { UserConfig as VitestUserConfig } from 'vitest/config';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+declare module 'vite' {
+	export interface UserConfig {
+		test: VitestUserConfig['test'];
+	}
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	test: {
+		globals: true,
+		environment: 'jsdom',
+		setupFiles: './src/tests/setupTest.ts',
+	},
+});
